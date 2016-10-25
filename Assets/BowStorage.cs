c989e6fe54b0506b2fde7cc7909926b7c4a4ec0b
Anthony <scatchell@gmail.com>
2016-10-25 15:54:10 +0100

    Attempting to store bow more properly using vrtk

--- Assets/BowStorage.cs
+++ Assets/BowStorage.cs
@@ -4,35 +4,53 @@ using System.Collections;
 namespace VRTK.Examples.Archery
 {
 	public class BowStorage : MonoBehaviour {
+		public bool stored = false;
 		// Use this for initialization
 		void Start () {
 			
 		}
 
 		void Update() {
-			
 		}
 		// Update is called once per frame
 		void OnTriggerStay(Collider other) {
 			if (other.gameObject.CompareTag ("Bow")) {
 				GameObject bow = other.gameObject;
-				if (!bow.GetComponent<BowAim> ().IsHeld ()) {
-					Debug.Log ("Inside storage");
-					bow.transform.parent = gameObject.transform;
-					//bow.transform.rotation = gameObject.transform.rotation;
+
+				bool bowBeingHeld = bow.GetComponent<BowAim> ().IsHeld ();
+				VRTK_InteractGrab grabbingController = (bow.GetComponent<VRTK_InteractGrab>() ? bow.GetComponent<VRTK_InteractGrab>() : bow.GetComponentInParent<VRTK_InteractGrab>());
+
+				if (grabbingController && grabbingController.gameObject.GetComponent<VRTK_ControllerEvents>().triggerPressed && !stored) {
+					grabbingController.ForceRelease();
 
 					bow.GetComponent<VRTK_InteractableObject>().ToggleKinematic(true);
+					bow.transform.parent = gameObject.transform;
+
+					stored = true;
+
+					Debug.Log ("Inside storage");
 				}
 			}
 		}
 
+		private bool HasBowAsChild()
+		{
+			return (gameObject.transform.FindChild("BasicBow") != null);
+		}
+
 		void OnTriggerExit(Collider other) {
 			if (other.gameObject.CompareTag ("Bow")) {
-				Debug.Log ("exiting.....");
-
 				GameObject bow = other.gameObject;
-				bow.transform.parent = null;
-				bow.GetComponent<VRTK_InteractableObject>().ToggleKinematic(false);
+				VRTK_InteractGrab grabbingController = (bow.GetComponent<VRTK_InteractGrab>() ? bow.GetComponent<VRTK_InteractGrab>() : bow.GetComponentInParent<VRTK_InteractGrab>());
+
+				if (grabbingController != null && stored)
+				{
+					//bow.transform.parent = null;
+					//bow.GetComponent<VRTK_InteractableObject>().ToggleKinematic(false);
+					stored = false;
+
+					Debug.Log ("Released from storage");
+				}
 			}
 		}
 	}
